cf349da460c5bd9c82e7a87a6b5ab0bbefb67b8c
Muhammad Arafat <3rafat@gmail.com>
2016-11-08 13:04:20 -0500

    demonstrate the use of lib with example

--- app/src/main/java/com/example/debuggablecmakeexample/MainActivity.java
+++ app/src/main/java/com/example/debuggablecmakeexample/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.debuggablecmakeexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.mylibrary.MyLibraryManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mHelloTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHelloTextView = (TextView) findViewById(R.id.hello_textView);
+        mHelloTextView.setText(MyLibraryManager.getInstance().getMsgFromNative());
     }
 }
